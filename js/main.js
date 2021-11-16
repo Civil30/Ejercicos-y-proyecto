2493f52099d45430c6178e55777115a602ed4e12
Civil30 <93553307+Civil30@users.noreply.github.com>
2021-11-15 23:13:58 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,65 +1,92 @@
-let entrada = prompt("consultar por Marta, Joaquin, Jorge, Oscar O Carla").toLowerCase();
+const vecinos = [];
+const pisos = [1, 2, 3, 4, 5, 6];
+const letras = ["A", "B", "C", "D"];
+
+
+
+const html = {
+    id : document.querySelector("#formulario"),
+    inputNombre : document.querySelector("#nombre"),
+    inputApellido : document.querySelector("#apellido"),
+    inputPisoN : document.querySelector("#pisoN"),
+    inputPisoL : document.querySelector("#pisoL"),
+    inputPago : document.querySelector("#pago"),
+    inputFecha : document.querySelector("#fecha"),
+    btnCrear : document.querySelector("#crear"),
+    btnMostrar : document.querySelector("#mostrar"),
+    inquilinos : document.querySelector(".inquilinos")
+};
+
 
 class vecino {
-    constructor (id, piso, nombre, pago, fecha){
+    constructor (id, pisoN, pisoL, nombre, apellido, pago, fecha){
         this.id = id;
-        this.piso = piso;
+        this.pisoN = pisoN;
+        this.pisoL = pisoL;
         this.nombre = nombre;
+        this.apellido = apellido;
         this.pago = pago;
         this.fecha = fecha;
     }
+};
+
+
+
+for (let i = 0; i < pisos.length; i += 1) {
+    const option = document.createElement("OPTION");
+    option.textContent = pisos[i]
+    html.inputPisoN.appendChild(option)
+    // html.inputPisoN.innerHTML += `<option>${pisos[i]}</option>`    
+}
+for (let i = 0; i < letras.length; i += 1) {
+    // const option = document.createElement("OPTION");
+    // option.textContent = letras[i]
+    // html.inputPisoL.appendChild(option)
+    html.inputPisoL.innerHTML += `<option>${letras[i]}</option>`    
 }
 
-const vecinos = [];
 
-function vecinosAdd(id, piso, nombre, pago, fecha) {
-    vecinos.push(new vecino (id, piso, nombre, pago, fecha));
-}     
 
-vecinosAdd(1, "1 A", "Marta", true, "01/08/2020");
-vecinosAdd(2, "1 B", "Joaquin", true, "22/05/2021");
-vecinosAdd(3, "2 A", "JORGE", false, "08/12/2020");
-vecinosAdd(4, "2 B", "oscar", true, "01/02/2019");
-vecinosAdd(5, "3 B", "Carla", false, "07/10/2021");
-
-// console.table(vecinos);  
-// console.log(vecinos);
+html.btnCrear.addEventListener("click", crear);
+html.btnMostrar.addEventListener("click", mostrarInquilinos);
 
 
 
-const deudores = vecinos.filter(nombre => nombre.pago === false);
+function vecinosAdd(id, pisoN, pisoL, nombre, apellido, pago, fecha) {
+    vecinos.push(new vecino (id, pisoN, pisoL, nombre, apellido, pago, fecha));
+};
 
-console.log(deudores);
+vecinosAdd(1, 1, "A", "Marta", "Juarez", true, "01/08/2020");
+vecinosAdd(2, 1, "B", "Joaquin", "Gomez", true, "22/05/2021");
+vecinosAdd(3, 2, "A", "JORGE", "RAMOS", false, "08/12/2020");
+vecinosAdd(4, 2, "B", "oscar", "diaz", true, "01/02/2019");
+vecinosAdd(5, 3, "B", "Carla", "Pirinch", false, "07/10/2021");
 
+function crear (evt) {
+    evt.preventDefault();    
+    const id = vecinos.length + 1;
+    const nombre = html.inputNombre.value;
+    const apellido = html.inputApellido.value;
+    const pisoN = html.inputPisoN.value;
+    const pisoL = html.inputPisoL.value;
+    const pago = html.inputPago.checked;
+    const fecha = html.inputFecha.value;
 
-vecinos.sort ((a,b) => {
+    vecinosAdd(id, pisoN, pisoL, nombre, apellido, pago, fecha)
+
+    console.log(vecinos);
+}
+
+function mostrarInquilinos() {
     
-    let nombreA = a.nombre.toLowerCase();
-    let nombreB = b.nombre.toLowerCase();
-    
-    if (nombreA < nombreB){
-        return -1;
-    }
-    
-    if (nombreA > nombreB){
-        return 1;
-    }
-    return 0;
-});
+    vecinos.forEach(persona => {
+        const div = document.createElement("DIV");
+        div.classList.add("card")
+        div.innerHTML = `Nombre: ${persona.nombre} ${persona.apellido},  Piso: ${persona.pisoN + persona.pisoL}`
+        html.inquilinos.appendChild(div)
+    })
 
-console.table(vecinos);
-
-
-vecinos.forEach(vecinos  => {
-    if (entrada == vecinos.nombre.toLowerCase()){
-        for (const prop in vecinos) {
-            document.write(`<p id="datos"> ${prop} = ${vecinos [prop] } </p>`);
-        }
-        
-        console.table(vecinos);
-    }
-});
-
+}
 
 
 
