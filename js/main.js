e36193b0903a3133ddbf2f35becc63b52e2b58cf
Civil30 <carly_arteaga@hotmail.com>
2021-12-03 02:16:56 -0300

    lo básico

--- js/main.js
+++ js/main.js
@@ -1,40 +1,51 @@
-const carga = $(".carga");
-const btn = $(".iniciar");
+const inputBuscar = $("#input__buscar");
+const btnBuscar = $("#btn__buscar")
+const harryUrl = "http://hp-api.herokuapp.com/api/characters"
 
+const harryDatos = () => {
+    $.get(harryUrl, (datos, estado)  => {
+        if(estado === "success") {
+            mostrarCarta (datos)
+        }
 
-$(document).ready(()=>{    
-    btn.click(() => {
-        carga.animate({
-            margin: "37rem 0 0 10rem"
-        },800)
-        .animate({
-            margin: "10rem 0 0 15rem"
-        },800)
-        .animate({
-            margin: "37rem 0 0 20rem"
-        },700)
-        .animate({
-            margin: "20rem 0 0 24rem"
-        },650)
-        .animate({
-            margin: "37rem 0 0 27rem"
-        },580)
-        .animate({
-            margin: "30rem 0 0 31rem"
-        },530)
-        .animate({
-            margin: "37rem 0 0 35rem"
-        },500)
-        .animate({
-            margin: "35rem 0 0 37rem"
-        },450)
-        .animate({
-            margin: "37rem 0 0 36.5rem"
-        },440)
-        .delay(2000)
-        carga.animate({
-            margin: "0 0 37rem 0"
-        })
     })
+}
+
+function mostrarCarta (datos) {
     
-})        
+    $(".personajes").empty()
+
+    datos.forEach(ele => {        
+        const {name, species, gender, house, dateOfBirth, image, actor} = ele;
+
+        if (inputBuscar.val().toLowerCase() == name.toLowerCase()) {
+            
+            $(".personajes").prepend(`<div class="card">
+                                        <h2>${name}</h2>
+                                        <img src=${image} alt="imagen">
+                                        <ul>
+                                            <li>Species:  ${species}</li>
+                                            <li>Gender:  ${gender}</li>
+                                            <li>House:	${house}</li>
+                                            <li>DateOfBirth:  ${dateOfBirth}</li>
+                                            <li>Actor:  ${actor}</li>
+                                        </ul>
+                                    </div>`)
+            console.log(ele)
+
+        }
+        
+    })
+}
+
+inputBuscar.on("input", () => {
+    
+    inputBuscar.val()
+}
+
+)
+
+btnBuscar.click(harryDatos)
+
+
+
